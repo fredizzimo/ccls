ce722dbd6ad3f5c0781085551a1c28b2dff08364
Fred Sundvik <fsundvik@gmail.com>
2021-08-20 13:42:05 +0300

    Add initial fzf support to workspace symbol

--- src/messages/workspace.cc
+++ src/messages/workspace.cc
@@ -8,6 +8,9 @@
 #include "project.hh"
 #include "query.hh"
 #include "sema_manager.hh"
+extern "C" {
+#include "fzf.h"
+}
 
 #include <llvm/ADT/STLExtras.h>
 #include <llvm/ADT/StringRef.h>
@@ -17,6 +20,7 @@
 #include <ctype.h>
 #include <functional>
 #include <limits.h>
+#include <memory>
 using namespace llvm;
 
 namespace ccls {
@@ -105,10 +109,10 @@ void MessageHandler::workspace_didChangeWorkspaceFolders(
 
 namespace {
 // Lookup |symbol| in |db| and insert the value into |result|.
-bool addSymbol(
-    DB *db, WorkingFiles *wfiles, const std::vector<uint8_t> &file_set,
-    SymbolIdx sym, bool use_detailed,
-    std::vector<std::tuple<SymbolInformation, int, SymbolIdx>> *result) {
+template <typename Adder>
+bool addSymbol(DB *db, WorkingFiles *wfiles,
+               const std::vector<uint8_t> &file_set, SymbolIdx sym,
+               Adder adder) {
   std::optional<SymbolInformation> info = getSymbolInfo(db, sym, true);
   if (!info)
     return false;
@@ -138,8 +142,115 @@ bool addSymbol(
   if (!ls_location)
     return false;
   info->location = *ls_location;
-  result->emplace_back(*info, int(use_detailed), sym);
+  adder(std::move(*info));
   return true;
+}
+
+template <typename RandomIt, typename Compare>
+void sift_down(RandomIt begin, RandomIt end, const Compare &comp = {}) {
+  const auto length = static_cast<size_t>(end - begin);
+  size_t current = 0;
+  size_t next = 2;
+  while (next < length) {
+    if (comp(*(begin + next), *(begin + (next - 1))))
+      --next;
+    if (!comp(*(begin + current), *(begin + next)))
+      return;
+    std::iter_swap(begin + current, begin + next);
+    current = next;
+    next = 2 * current + 2;
+  }
+  --next;
+  if (next < length && comp(*(begin + current), *(begin + next)))
+    std::iter_swap(begin + current, begin + next);
+}
+
+template <typename T, typename Deleter>
+decltype(auto) make_unique(T *p, Deleter deleter) {
+  return std::unique_ptr<T, Deleter>(p, deleter);
+}
+
+void workspace_symbol_fzf(MessageHandler &handler, const std::string &query,
+                          const std::vector<uint8_t> &file_set,
+                          ReplyOnce &reply) {
+
+  const fzf_case_types case_modes[] = {case_ignore, case_smart, case_respect};
+  fzf_case_types case_mode =
+      case_modes[g_config->workspaceSymbol.caseSensitivity];
+  const bool normalize = false;
+  const bool fuzzy = true;
+  fzf_string_t fzf_query = {query.data(), query.size()};
+  auto fzf_pattern = make_unique(
+      fzf_parse_pattern_str(case_mode, normalize, &fzf_query, fuzzy),
+      [](auto *p) { fzf_free_pattern(p); });
+  auto slab =
+      make_unique(fzf_make_default_slab(), [](auto *p) { fzf_free_slab(p); });
+
+  auto *db = handler.db;
+  const size_t max_num = std::min<size_t>(g_config->workspaceSymbol.maxNum,
+                                          std::numeric_limits<uint32_t>::max());
+  std::vector<SymbolInformation> cands;
+  std::vector<std::tuple<int32_t, uint32_t>> cands_heap;
+  cands.reserve(max_num);
+  cands_heap.reserve(max_num);
+
+  auto add = [&](SymbolIdx sym) {
+    std::string_view detailed_name = handler.db->getSymbolName(sym, true);
+    if (detailed_name.size() > 0) {
+      fzf_string_t name = {detailed_name.data(), detailed_name.size()};
+      auto score = fzf_get_score_str(&name, fzf_pattern.get(), slab.get());
+      if (score > 0) {
+        if (cands.size() < max_num) {
+          addSymbol(db, handler.wfiles, file_set, sym,
+                    [&cands, &cands_heap, &score](SymbolInformation &&info) {
+                      cands_heap.emplace_back(score, cands.size());
+                      cands.emplace_back(std::move(info));
+                    });
+        } else {
+          if (cands.size() == max_num) {
+            std::make_heap(cands_heap.begin(), cands_heap.end(),
+                           [](const auto &lhs, const auto &rhs) {
+                             return std::get<0>(lhs) > std::get<0>(rhs);
+                           });
+          }
+          auto &max_element = cands_heap[0];
+          auto &max_score = std::get<0>(max_element);
+          if (score > max_score) {
+            addSymbol(db, handler.wfiles, file_set, sym,
+                      [&](SymbolInformation &&info) {
+                        max_score = score;
+                        auto &sym = cands[std::get<1>(max_element)];
+                        sym = std::move(info);
+                        sift_down(cands_heap.begin(), cands_heap.end(),
+                                  [&](const auto &lhs, const auto &rhs) {
+                                    const auto &sl = std::get<0>(lhs);
+                                    const auto &sr = std::get<0>(rhs);
+                                    return sl > sr;
+                                  });
+                      });
+          }
+        }
+      }
+    }
+  };
+  for (auto &func : db->funcs)
+    add({func.usr, Kind::Func});
+  for (auto &type : db->types)
+    add({type.usr, Kind::Type});
+  for (auto &var : db->vars)
+    if (var.def.size() && !var.def[0].is_local())
+      add({var.usr, Kind::Var});
+
+  std::sort(cands_heap.begin(), cands_heap.end(),
+            [](const auto &l, const auto &r) { return l > r; });
+  std::vector<SymbolInformation> result;
+  result.reserve(cands_heap.size());
+  for (const auto &i : cands_heap) {
+    auto &info = cands[std::get<1>(i)];
+    result.push_back(std::move(info));
+  }
+
+  reply(result);
 }
 } // namespace
 
@@ -150,6 +261,9 @@ void MessageHandler::workspace_symbol(WorkspaceSymbolParam &param,
   for (auto &folder : param.folders)
     ensureEndsInSlash(folder);
   std::vector<uint8_t> file_set = db->getFileSet(param.folders);
+  const bool use_fzf = g_config->workspaceSymbol.sort;
+  if (use_fzf)
+    return workspace_symbol_fzf(*this, query, file_set, reply);
 
   // {symbol info, matching detailed_name or short_name, index}
   std::vector<std::tuple<SymbolInformation, int, SymbolIdx>> cands;
@@ -167,8 +281,11 @@ void MessageHandler::workspace_symbol(WorkspaceSymbolParam &param,
     int pos = reverseSubseqMatch(query_without_space, detailed_name, sensitive);
     return pos >= 0 &&
            addSymbol(db, wfiles, file_set, sym,
-                     detailed_name.find(':', pos) != std::string::npos,
-                     &cands) &&
+                     [&](SymbolInformation &&info) {
+                       const bool detailed =
+                           detailed_name.find(':', pos) != std::string::npos;
+                       cands.emplace_back(std::move(info), detailed, sym);
+                     }) &&
            cands.size() >= g_config->workspaceSymbol.maxNum;
   };
   for (auto &func : db->funcs)
@@ -181,8 +298,8 @@ void MessageHandler::workspace_symbol(WorkspaceSymbolParam &param,
     if (var.def.size() && !var.def[0].is_local() && add({var.usr, Kind::Var}))
       goto done_add;
 done_add:
-
-  if (g_config->workspaceSymbol.sort && query.size() <= FuzzyMatcher::kMaxPat) {
+  if (g_config->workspaceSymbol.sort &&
+      (query.size() <= FuzzyMatcher::kMaxPat)) {
     // Sort results with a fuzzy matching algorithm.
     int longest = 0;
     for (auto &cand : cands)
